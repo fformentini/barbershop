c780c1ad8f08b9a35c929c35f8bec42890d9da50
Fabrício Formentini <fabricioformentini2@gmail.com>
2025-08-22 12:52:28 -0300

    feat(responsive): make layout responsive for mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// evento para deixar o navbar transparente ao rolar
 const navbar = document.getElementById('navbar');
 
 window.addEventListener('scroll', () => {
@@ -7,3 +8,25 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('transparent'); // volta ao sólido no topo
   }
 });
+
+
+// Menu hamburguer para mobile
+const hamburgerBtn = document.querySelector('.hamburger');
+const nav = document.querySelector('.navbar');
+const menuLinks = document.querySelectorAll('.navbar a');
+
+function closeNavbar() {
+  nav.classList.remove('open');
+}
+
+if (hamburgerBtn) {
+  hamburgerBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+  // Fecha o menu ao clicar em um link
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      closeNavbar();
+    });
+  });
+}
